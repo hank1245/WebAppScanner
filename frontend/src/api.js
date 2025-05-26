0ec15a38ddce4910729a704a0538507f96728bfc
hank <hank29206880@gmail.com>
2025-05-26 18:12:46 +0900

    chore: add API_URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL ||
+  "friendly-integrity-production-dcec.up.railway.app:8000";
 
 export const scanWebsite = async (
   targetUrls,
