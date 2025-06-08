7e404a45dfd37c40630c70a451a5e8f6b85e02d3
hank <hank29206880@gmail.com>
2025-06-08 22:18:36 +0900

    fix: change App.test.js depth

--- frontend/App.test.js
+++ frontend/App.test.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import App from "../App";
+import App from "./App.js";
 
 // API 모킹
-jest.mock("../api");
+jest.mock("./api.js");
 
 describe("App", () => {
   test("renders main components", () => {
